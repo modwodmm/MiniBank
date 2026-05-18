07470db1bff841483c694a163d4387c4b2603cee
modwodmm <modwodmm2010@gmail.com>
2026-05-18 22:03:19 +0500

    Used array lists instead of arrays

--- untitled1/src/Bank.java
+++ untitled1/src/Bank.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bank {
 
 	private Scanner scanner = new Scanner(System.in);
-	private Account[] accounts = new Account[5];
+	private ArrayList<Account> accounts = new ArrayList<>();
 	
-	private int count = 0;
 	private Account currentAccount;
 	private boolean success = false;
 	
 //Menu	
 	public void menu() {
 		while(true) {
-			if(count == 0) {
+			if(accounts.size() == 0) {
 				System.out.println("There are no accounts.\n1. Create new account");
 			}
 			else {
@@ -43,39 +43,46 @@ public class Bank {
 //Creates account	
 	public void createAccount() {
 		
-		if(count >= accounts.length) {
+		if(accounts.size() >= 5) {
 			
 			System.out.println("You hit the account limit!");
 			
 		}
 		else {
 			
+			boolean exists = false;
+			
 			System.out.println("Enter the username:");
 			String name = scanner.next();
 			
-			System.out.println("Enter the pin:");
-			int pin = scanner.nextInt();
-			
-			double balance;
-			
-			while(true) {
-				System.out.println("Enter the starting amount:");
-				balance = scanner.nextDouble();
-				if(balance > 0 && balance <= 100) {
-					break;
-				}
-				System.out.println("Starting balance can't be negative or above 100");
-			}
-			
-			for(int i = 0; i < accounts.length; i++) {
-				if(accounts[i] == null) {
-					accounts[i] = new Account(name, pin, balance);
-					currentAccount = accounts[i];
+			for(int i = 0; i < accounts.size(); i++) {
+				if(accounts.get(i).getUsername().equalsIgnoreCase(name)) {
+					exists = true;
 					break;
 				}
 			}
-			count++;
-			success = true;
+			if(exists) {
+				System.out.println("Account already exists!!");
+			}
+			else {
+				
+				System.out.println("Enter the pin:");
+				int pin = scanner.nextInt();
+				
+				double balance;
+				
+				while(true) {
+					System.out.println("Enter the starting amount:");
+					balance = scanner.nextDouble();
+					if(balance > 0 && balance <= 100) {
+						break;
+					}
+					System.out.println("Starting balance can't be negative or above 100");
+				}
+				accounts.add(new Account(name, pin, balance));
+				System.out.println("New account has been added.");
+				success = true;
+			}
 		}
 	}
 	
@@ -86,18 +93,18 @@ public class Bank {
 		
 		boolean found = false;
 		
-		for(int i = 0; i < accounts.length; i++) {
+		for(int i = 0; i < accounts.size(); i++) {
 			
-			if(accounts[i] != null && accounts[i].getUsername().equalsIgnoreCase(target)) {
+			if(accounts.get(i).getUsername().equalsIgnoreCase(target)) {
 				found = true;
 				int tries = 0;
 				
 				while(tries < 3) {
 					System.out.println("Enter your pin:");
 					int enteredPin = scanner.nextInt();
-					if(accounts[i].checkPin(enteredPin)) {
+					if(accounts.get(i).checkPin(enteredPin)) {
 						System.out.println("Logged in successfully!");
-						currentAccount = accounts[i];
+						currentAccount = accounts.get(i);
 						success = true;
 						return;
 					}
